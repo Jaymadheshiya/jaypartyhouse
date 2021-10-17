99f869f5d4aa2a5f17b56e0e0e5312cb5b42af3a
jay <govindjay39@gmail.com>
2021-10-18 00:09:56 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Akronim&display=swap" rel="stylesheet"> -->
+
+    <title>Project1</title>
+</head>
+<body>
+    
+ 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Jai party House</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="About.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Services.php">Services</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href=""Contact.php>Contact</a>
+      </li>
+      
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>jai party house</h1>
+  <p>plse like and share and subscribe</p>
+</div>
+
+<section class="my-5">
+    <div class="py-5" >
+      <h1 class="text-center">About Us</h1>
+    </div>
+    <div class="container-fluid ">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <img src="images/jai.jpg" alt="error" class="img-fluid aboutimg" >
+
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <h1 class="text-center display-4">This is jai party house</h1>
+            <p class="text-center py-2">Hi! guys this is the party website here only they are allowed only failure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum officiis odit sapiente nostrum modi quas, veritatis iure fugit incidunt repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vero dolorum culpa libero laborum harum quaerat similique earum alias officiis, eum at itaque, a provident inventore, tempore quam. In, blanditiis. </p>
+           <a href="About.php" class="btn btn-success"> check more  </a>
+        </div>
+    </div>
+    </div>
+</section>
+<footer >
+    <p class="p-3 bg-dark text-white text-center">@jaypartyhouseproductionpresenting...</p>
+
+  </footer>
+    
+ </body>
+ </html>
